b0c704ff4addb3f8ae7edb45bd5ed3d1a8301cb4
MayraLM90 <mayralm87@gmail.com>
2022-04-26 15:59:41 -0300

    Se agrega el modulo express, el app.js, cambio de ubicación del logo a carpeta images, y cambios en el index.html

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+
+const app = express();
+
+const path = require('path');
+
+let port = 3030
+
+app.listen (port, () => console.log("Servidor andando"))
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve('./views/index.html'))
+});
+app.get('/home', (req, res) =>{
+    res.sendFile(path.resolve('./views/index.html'))
+});
+
+app.get('/index', (req, res) =>{
+    res.sendFile(path.resolve('./views/index.html'))
+});
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve('./views/login.html'))
+});
+
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve('./views/register.html'))
+});
+
+app.get('/productDetail', (req, res) =>{
+    res.sendFile(path.resolve('./views/productDetail'))
+});
+
+app.get('/productCart', (req, res) =>{
+    res.sendFile(path.resolve('./views/productCart.html'))
+});
