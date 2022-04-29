59902ef02eae128a2ce0d3c066ca8f8c3c8880d2
MayraLM90 <mayralm87@gmail.com>
2022-04-29 13:05:47 -0300

    Se agrega estructura al html de login y register

--- app.js
+++ app.js
@@ -25,7 +25,15 @@ app.get('/login', (req, res) =>{
     res.sendFile(path.resolve('./views/login.html'))
 });
 
+app.post('/login', (req, res) =>{
+    res.sendFile(path.resolve('./views/login.html'))
+});
+
 app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve('./views/register.html'))
+});
+
+app.post('/register', (req, res) =>{
     res.sendFile(path.resolve('./views/register.html'))
 });
 
